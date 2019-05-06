ed08bbf75a815ec9f30ecb2b2dfcf596344efe21
lukassabinoo <lukassabino_@hotmail.com>
2019-05-06 14:47:08 -0300

    Arquivos do projeto

---
+++ README.md
@@ -0,0 +1,15 @@
+# Projeto Simples em Java
+
+Projeto de exemplo utilizando maven
+
+## Java Version
+
+1.8
+
+## Encode
+
+UTF-8
+
+## Testes
+
+JUnit 4.12
---
+++ src/main/java/br/com/edward/app_basica/App.java
@@ -0,0 +1,9 @@
+package br.com.edward.app_basica;
+
+public class App {
+	
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
